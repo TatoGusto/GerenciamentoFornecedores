4a88dc74a8b77ad99f11cee0d29a9733463a8797
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 17:23:34 -0300

    service criado

---
+++ GerenciamentoFornecedores/src/main/java/com/oasw/GerenciamentoFornecedores/Services/FornecedorService.java
@@ -0,0 +1,30 @@
+package com.oasw.GerenciamentoFornecedores.Services;
+import com.oasw.GerenciamentoFornecedores.Models.FornecedorModel;
+import com.oasw.GerenciamentoFornecedores.Repositories.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FornecedorService {
+
+    @Autowired
+    FornecedorRepository fornecedorRepository;
+
+    public FornecedorModel create(FornecedorModel fornecedorModel){
+        return fornecedorRepository.save(fornecedorModel);
+    }
+
+    public List<FornecedorModel> getAll(){
+        return fornecedorRepository.findAll();
+    }
+
+    public FornecedorModel getById(Long id){
+        return fornecedorRepository.getById(id);
+    }
+
+    public void deleteById(Long id){
+        fornecedorRepository.deleteById(id);
+    }
+}
